ef005614e021f87412540478acf7547796ca5c30
wbran27 <brangomez@verizon.net>
2025-04-23 17:44:43 -0600

    changed the display name

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // this is a program to compute radius from a circle.
+
+        // get radius
+        Console.Write("please enter the radius: ");
+        string text = Console.ReadLine();
+        // convert string into double (integer)
+        double radius = double.Parse(text);
+
+        // compute are of the circle
+        double area = Math.PI * radius * radius;
+
+        // display area of user to see
+        Console.WriteLine($"area of the circle {area}");
+        
+        int x = 3;
+        string s = "sup";
+        float f = 3.14F;
+        double d = 5.21;
+        // $ works like f like in python 
+        Console.WriteLine($"Hello, {s} Sandbox World! {x} {f} {d}");
     }
 }
