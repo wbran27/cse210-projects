cc9590ea4fbdfc7071e56eaff0a810dc0597a414
wbran27 <brangomez@verizon.net>
2025-04-30 09:25:19 -0600

    d

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+    
+        Console.Write("what it the magic number: ");
+        int magic = int.Parse(Console.ReadLine());
+        Console.Write("what is your guess: ");
+        int guess = int.Parse(Console.ReadLine());
+
+        string hint = "";
+        while (guess != magic)
+        {
+            if (guess < magic)
+            {
+                hint = "higher";
+            }
+            else if (guess > magic)
+            {
+                hint = "lower";
+            }
+            else if (guess == magic)
+            {
+                hint = "correct guess";
+            }
+            else
+            {
+                hint = "incorrect input";
+            }
+            Console.WriteLine($"hint: {hint}");
+            Console.Write("try again: ");
+            guess = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("You won, you guessed correctly");
+
+
     }
 }
