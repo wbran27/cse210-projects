40f0d49debe6f8e6bfe16a5160d8f92c28595bb6
wbran27 <brangomez@verizon.net>
2025-05-19 09:51:23 -0600

    d

--- sandbox/Sandbox/Account.cs
+++ sandbox/Sandbox/Account.cs
@@ -1,6 +1,34 @@
 namespace ConsoleApp1;
 
-public class Class1
-{
-    
+public class Account
+{   
+    private list<int> _transactions = new list<int>();
+
+    private int _balance = int.MinValue;
+
+    public void deposit(int amount)
+    {
+        _transactions.Add(ammount);
+        _balance = int.Minvalue;
+    }
+
+    public void SetBalance(int balance)
+    {
+        _balance = balance;2
+    }
+
+    public int GetBalance()
+    {
+        if (_balanve == int.MinValue)
+        {
+            int runningBalance = 0;
+            foreach (int amount in _transactions)
+            {
+                runningBalance += amount;
+            }
+        }
+        return _balance;
+    }
+
+
 }
