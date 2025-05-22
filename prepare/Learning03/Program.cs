a1796b22fd81e471e695e55cbd87e9ea861bc69d
wbran27 <brangomez@verizon.net>
2025-05-21 19:01:42 -0600

    d

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Console.WriteLine("fractions program");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(3, 4);
+        console.WriteLine("fractions program");
+        console.WriteLine(f1.GetFractionSrtring());
+        console.WriteLine(f1.GetDecimalValue());
+        console.WriteLine(f2.GetFractionString());
+        console.WriteLine(f2.GetDecimalValue());
     }
 }
